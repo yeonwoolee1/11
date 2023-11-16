6eb2415b372eb078a9982b64eceb0305a5487098
yeonwoolee1 <144992849+yeonwoolee1@users.noreply.github.com>
2023-11-16 15:31:23 +0900

    실습2

--- main.c
+++ main.c
@@ -4,21 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i=10;
-	char c='a';
+	int i = 300;
 	
-	int* iptr; 
-	iptr = &i;
+	int *pi = &i; 
+	char *pc = &i;
 	
-	char* cptr;
-	cptr = &c; 
-	
-	int* iptr2;
-	iptr2=iptr;
-	
-	printf("i : %p\n%p (size:%i)\n", iptr, &i, sizeof(iptr)); 
-	printf("c : %p\n%p (size:%i)\n", cptr, &c, sizeof(cptr)); 
-	printf("iptr2 : %p, %i\n", iptr2, *iptr2);
-
+	printf("%i, %i, %i\n", i, *pi, *pc); 
+	 
 	return 0;
 }
