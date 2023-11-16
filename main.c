2588c8ade70c97f4f25ea8e69ba5f0961bbe567d
yeonwoolee1 <144992849+yeonwoolee1@users.noreply.github.com>
2023-11-16 15:36:23 +0900

    실습3

--- main.c
+++ main.c
@@ -2,14 +2,18 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void swap (int *x, int *y) { 
+	int temp; 
+	temp = *x; 
+	*x = *y; 
+	*y = temp;
+}
 
-int main(int argc, char *argv[]) {
-	int i = 300;
-	
-	int *pi = &i; 
-	char *pc = &i;
-	
-	printf("%i, %i, %i\n", i, *pi, *pc); 
-	 
+void main(void) {
+	int a = 3; 
+	int b = 5; 
+	swap(&a, &b); 
+	printf("a:%i, b:%i\n", a, b); 
 	return 0;
 }
+
