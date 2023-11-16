b08e83b1bf96dfdc1c5d12fd1ece65360a41c620
yeonwoolee1 <144992849+yeonwoolee1@users.noreply.github.com>
2023-11-16 16:07:44 +0900

    실습5

--- main.c
+++ main.c
@@ -4,23 +4,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	int i; 
-	int grade[5];
-	int average=0;
-	 
-	for (i=0;i<5;i++) 
-	{ 
-		printf("input value[%i]= " , i); 
-		scanf("%d", &grade[i]); 
-	}
+	char *pc; 
+	int *pi; 
+	double *pd; 
 	
-	for (i=0;i<5;i++) 
-	{ 
-	printf("grade[%i] = %i\n", i, *(grade+i));
-	average=average+ *(grade+i);
-    }
+	pc = (char *)10000; 
+	pi = (int*)10000; 
+	pd = (double *)10000;
+	
+	printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd); 
 
-	printf("average: %i\n", average/5); 
+	pc++; 
+	pi++; 
+	pd++; 
+	printf("증가 후 : pc = %d, pi = %d, pd = %d\n" , pc, pi, pd); 
 	
 	return 0;
 }
